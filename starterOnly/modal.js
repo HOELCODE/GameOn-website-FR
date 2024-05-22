4038e852fa00d052138ca04e011b267b2133e413
HOELCODE <hoelpro@outlook.fr>
2024-05-23 00:41:19 +0200

    Maquette ok, bouton x ok, lien label-input ok

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,22 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeForm = document.querySelectorAll(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
+// Ecouter le clique sur le bouton fermer du modal et lui associer la fonction CloseModal
+closeForm.forEach((btn) => btn.addEventListener("click", closeModal));
+
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+}
+
+// Close modal form
+function closeModal() {
+  modalbg.style.display = "none";
 }
 
 
