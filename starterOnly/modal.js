d4479a7bdbd6235231671822e66f8cee5e79de74
HOELCODE <hoelpro@outlook.fr>
2024-06-02 23:38:33 +0200

    Veriification email ok

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', function () {
   const modalBtn = document.querySelectorAll(".modal-btn");
   const closeForm = document.querySelectorAll(".close");
   const inputs = document.querySelectorAll(".text-control");
-  const spanErreur = document.querySelector(".message-erreur")
+  const spanErreur = document.querySelectorAll(".message-erreur")
 
   // launch modal event
   modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -28,16 +28,46 @@ document.addEventListener('DOMContentLoaded', function () {
       const containsDigit = /\d/.test(input.value);
       const containsSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(input.value);
 
+      // Fonction emails 
+      function verifierEmail(email) {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+      }
+
       // Vérifier si l'input a l'ID "first" et verification contenu
       if (input.id === "first") {
         // Si il y'a chiffre ou caractère spécial alors message d'erreur SINON pas de message d'erreur
         if (containsDigit || containsSpecialChar) {
-          spanErreur.style.display = "inline";
-          spanErreur.textContent = "Vous ne pouvez pas entrer de chiffres ou caractères spéciaux"
+          spanErreur[0].style.display = "inline";
+          spanErreur[0].textContent = "Vous ne pouvez pas entrer de chiffres ou caractères spéciaux"
           input.style.border = "2px solid red";
         } else {
-          spanErreur.style.display = "none";
+          spanErreur[0].style.display = "none";
           input.style.border = "";
+        }
+      } else if (input.id === "last") {
+        // Si il y'a chiffre ou caractère spécial alors message d'erreur SINON pas de message d'erreur
+        if (containsDigit || containsSpecialChar) {
+          spanErreur[1].style.display = "inline";
+          spanErreur[1].textContent = "Vous ne pouvez pas entrer de chiffres ou caractères spéciaux"
+          input.style.border = "2px solid red";
+        } else {
+          spanErreur[1].style.display = "none";
+          input.style.border = "";
+        }
+      } else if (input.id === "email") {
+        const emailVerification = verifierEmail(input.value);
+
+        // Vérifie si l'adresse e-mail est valide et ne contient pas de caractères spéciaux ou de chiffres
+        if (!emailVerification) {
+          // Affiche le message d'erreur pour une adresse e-mail invalide
+          spanErreur[2].style.display = "inline";
+          spanErreur[2].textContent = "Adresse e-mail invalide";
+          input.style.border = "2px solid red";
+        } else {
+          // Cache le message d'erreur si l'adresse e-mail est valide
+          spanErreur[2].style.display = "none";
+          input.style.border = ""; // Réinitialise la bordure
         }
       }
     });
